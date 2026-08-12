2fe489a57dc7a8b2c0026db7cee36b7adc5dc762
Ktiwari2310 <beingkt.23@gmail.com>
2026-08-12 23:35:30 +0530

    Backend

--- Backend/src/routes/expense.routes.js
+++ Backend/src/routes/expense.routes.js
@@ -11,9 +11,9 @@ const { authUser } = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
-router.post("/", authUser, addExpense);
-router.put("/:id", authUser, updateExpense);
-router.delete("/:id", authUser, deleteExpense);
+router.post("/addExpense", authUser, addExpense);
+router.put("/updateExpense/:id", authUser, updateExpense);
+router.delete("/deleteExpense/:id", authUser, deleteExpense);
 router.get("/", authUser, getExpenses);
 
 module.exports = router;
